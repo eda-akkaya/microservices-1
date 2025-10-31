ecb1e45248aaab0a0636450d3ecd0f17997c3d7a
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-31 21:09:55 +0300

    build: add gateway-server

---
+++ product-service/src/main/java/com/example/product_service/ProductsController.java
@@ -0,0 +1,15 @@
+package com.example.product_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/products")
+@RestController
+public class ProductsController {
+
+    @GetMapping
+    public String get(){
+        return "Merhaba, Product Servcice";
+    }
+}
