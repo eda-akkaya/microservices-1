68a93d5d73735b69ac8b0a75e2e0f86277aa88b8
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-11-03 21:41:23 +0300

    build: add product client, openfeign

---
+++ order-service/src/main/java/com/example/order_service/controller/OrdersController.java
@@ -0,0 +1,23 @@
+package com.example.order_service.controller;
+
+import com.example.order_service.client.ProductClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrdersController {
+
+    private ProductClient productClient;
+
+    public OrdersController(ProductClient productClient){
+        this.productClient=productClient;
+    }
+
+    @GetMapping
+    public String get(){
+        String responseFromProductService = productClient.get();
+        return "order service çalıştı. product service'ten gelen cevap: " + responseFromProductService;
+    }
+}
